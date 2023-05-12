14277588f2fba21115cf010e972ba81a601627d1
AndreusMaximus <a.faesen@gmail.com>
2023-05-12 08:09:10 +0200

    improved test script

---
+++ scripts/CMakeFiles/runtests.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/aborts.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/aborts.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/basic.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/basic.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/fig1.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/fig1.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/fig1_dense.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/fig1_dense.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/fig1_prec.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/fig1_prec.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/global.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/global.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/global_prec.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/global_prec.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/iip.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/iip.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/io.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/io.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/por.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/por.cpp.o"
+  "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/src/tests/state_space.cpp" "/home/sag/Downloads/np-schedulability-analysis-partial_order_reduction/scripts/CMakeFiles/runtests.dir/src/tests/state_space.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- scripts/compare-script.sh
+++ scripts/compare-script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo "(1) BUILDING PHASE"
+cd ../build
+cmake --fresh ..
+make clean
+make -j
+cd ../scripts
+
+echo "(2) RUNNING COMPARISONS"
 truncate -s 0 result.txt
 for i in {0..999}
 do
@@ -6,3 +14,6 @@ do
     ../build/nptest -r -m 4 ~/Downloads/real-time-task-generators-main/jobsets/jobset-taskset-$i.csv >> result.txt
     ../build/nptest -r -m 4 ~/Downloads/real-time-task-generators-main/jobsets/jobset-taskset-$i.csv --por=priority  >> result.txt
 done
+
+echo "(3) PARSING RESULTS"
+python3 parse-results.py
